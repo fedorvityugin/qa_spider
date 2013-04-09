ecefd8ed36af7c8880348945dbad2fa7d27a044d
Fedor <fedor@fedor-VPCSB3M1R.(none)>
2013-04-09 18:56:31 +0400

    Add part of script for extract information from question page.

--- qa.py
+++ qa.py
@@ -4,8 +4,8 @@ import urllib
 import lxml.html
 
 def QuoraLinkSpider(adress):
-    page = urllib.urlopen(adress)
-    finPage = page.read()
+    newPage = urllib.urlopen(adress)
+    finPage = newPage.read()
     
     doc = lxml.html.document_fromstring(finPage.decode('utf-8'))
 
@@ -15,5 +15,18 @@ def QuoraLinkSpider(adress):
     
     for url in list_of_urls:
         return url
+        
+def QuoraQuestionExtractor(link):
+    endPage = urllib.urlopen(link)
+    finalPage = endPage.read()
+                
+    doc = lxml.html.document_fromstring(finalPage.decode('utf-8'))
+    
+    list_of_questions = []
+    for question in doc.cssselect('.h1'):
+        list_of_questions.append(question.text_content())
+        
+    for link in list_of_links:
+        return link
 
 QuoraLinkSpider('file:///home/fedor/Dropbox/Science/qa_spider/saved_pages/New%20Questions%20-%20Quora.html') #Сюда кидаем адрес страницы
