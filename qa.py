a29592904fc3cb6bb1a5adfc8bc04c563446296c
fedorvityugin <fedor.vityugin@gmail.com>
2013-11-17 02:32:43 +0400

    Refactored script

--- qa.py
+++ qa.py
@@ -1,27 +1,28 @@
 # -*- coding: utf-8 -*-
 
 import urllib
+
 import lxml.html
 
-def QuoraLinkSpider(adress):
-    newPage = urllib.urlopen(adress)
-    finPage = newPage.read()
+def grab_quora_link(adress):
+    new_page = urllib.urlopen(adress)
+    fin_page = new_page.read()
     
-    doc = lxml.html.document_fromstring(finPage.decode('utf-8'))
+    doc = lxml.html.document_fromstring(fin_page.decode('utf-8'))
 
     list_of_urls = []
     for question in doc.cssselect('.question_link'):
         list_of_urls.append(question.get('href'))
     
     for url in list_of_urls[:15]:
-        QuoraQuestionExtractor(url)    
+        extract_quora_question(url)
         
         
-def QuoraQuestionExtractor(link):
-    endPage = urllib.urlopen(link)
-    finalPage = endPage.read()
+def extract_quora_question(link):
+    end_page = urllib.urlopen(link)
+    final_page = end_page.read()
                 
-    doc = lxml.html.document_fromstring(finalPage.decode('utf-8'))
+    doc = lxml.html.document_fromstring(final_page.decode('utf-8'))
     
     list_of_questions = []
     for question in doc.cssselect('.w5.question_text_edit.row>h1'):
@@ -30,5 +31,5 @@ def QuoraQuestionExtractor(link):
     for link in list_of_questions:
         print link
 
-QuoraLinkSpider('file:///home/fedor/Dropbox/Science/qa_spider/saved_pages/New%20Questions%20-%20Quora.html') #Сюда кидаем адрес страницы
+grab_quora_link('file:///home/fedor/Dropbox/Science/qa_spider/saved_pages/New%20Questions%20-%20Quora.html') #Сюда кидаем адрес страницы
 
